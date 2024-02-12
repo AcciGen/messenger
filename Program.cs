406ea42418423688be47a0060037e35ab342376e
Nuriddin Asrorov <denrov13@gmail.com>
2024-02-12 08:26:58 +0500

    Some changes

--- Program.cs
+++ Program.cs
@@ -1,10 +1,24 @@
-﻿namespace messenger
+﻿using Npgsql;
+
+namespace messenger
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            const string CONNECTIONSTRING = "Server=localhost;Port=5432;Database=Assignment;User Id=postgres;Password=135;";
+
+            NpgsqlConnection connection = new NpgsqlConnection(CONNECTIONSTRING);
+            connection.Open();
+
+            query = $"Create table users (id bigserial primary key, username varchar(50) unique, password varchar(50)); Create table messengers (id bigserial primary key, user varchar(50), fromUser varchar(50), message text);";
+
+            NpgsqlCommand command = new NpgsqlCommand(query, connection);
+
+            command.ExecuteNonQuery();
+
+            connection.Close();
+            Console.WriteLine("Success in creating tables!");
         }
     }
 }
